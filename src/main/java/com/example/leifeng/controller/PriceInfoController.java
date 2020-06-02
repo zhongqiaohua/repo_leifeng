a33a1c002f62e6b7ebecb5f532b627af340a7c25
zhongqiaohua <953901509@qq.com>
2020-06-02 13:26:49 +0800

    添加PriceInfoController

---
+++ src/main/java/com/example/leifeng/controller/PriceInfoController.java
@@ -0,0 +1,13 @@
+package com.example.leifeng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@ResponseBody
+@RequestMapping("/priceInfo")
+public class PriceInfoController {
+    
+}
